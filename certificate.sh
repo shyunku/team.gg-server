55bede542336e5dacbb2fc0b770afe952308adee
shyunku <whdudgns7321@gmail.com>
2023-12-23 22:01:08 +0900

    update domain -> teamgg.kr

--- certificate.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p certificates
-sudo cp /etc/letsencrypt/live/team.gg/fullchain.pem certificates/cert.pem
-sudo cp /etc/letsencrypt/live/team.gg/privkey.pem certificates/key.pem
-sudo chmod 664 certificates/cert.pem
-sudo chmod 664 certificates/key.pem
--- set_cert.sh
+++ set_cert.sh
@@ -1,5 +1,5 @@
 mkdir -p certificates
-sudo cp /etc/letsencrypt/live/team-gg.net/fullchain.pem certificates/cert.pem
-sudo cp /etc/letsencrypt/live/team-gg.net/privkey.pem certificates/key.pem
+sudo cp /etc/letsencrypt/live/teamgg.kr/fullchain.pem certificates/cert.pem
+sudo cp /etc/letsencrypt/live/teamgg.kr/privkey.pem certificates/key.pem
 sudo chmod 664 certificates/cert.pem
 sudo chmod 664 certificates/key.pem
